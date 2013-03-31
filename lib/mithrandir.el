f89db34cddb66b45deada8d1e52d2e4d4817dd3a
Roth Michaels <roth@rothmichaels.us>
2013-03-31 10:48:06 -0400

    Added default terminal color setting to color-theme-mithrandir

--- lib/mithrandir.el
+++ lib/mithrandir.el
@@ -84,6 +84,15 @@
      (magit-diff-hunk-header ((t (:foreground "orange"))))
      (magit-branch ((t (:foreground "DarkGoldenRod"))))
      (magit-whitespace-warning-face ((t (:background "#fff3e9"))))
+
+     (term-color-black ((t (:background "black" :foreground "black"))))
+     (term-color-blue ((t (:background "blue2" :foreground "blue2"))))
+     (term-color-cyan ((t (:background "cyan3" :foreground "cyan3"))))
+     (term-color-green ((t (:background "green3" :foreground "green3"))))
+     (term-color-magenta ((t (:background "magenta3" :foreground "magenta3"))))
+     (term-color-red ((t (:background "red3" :foreground "red3"))))
+     (term-color-white ((t (:background "white" :foreground "white"))))
+     (term-color-yellow ((t (:background "yellow3" :foreground "yellow3"))))
      ))
   )
 
